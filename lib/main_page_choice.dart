fc3f2ce5ad44d5806ffac8d2a795e4a44bec7649
osiris <osiris@31337.local>
2021-12-09 15:34:12 +0900

    added main_page_chice.dart

---
+++ lib/main_page_choice.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MainPageChoiceApp extends StatefulWidget {
+  @override
+  MainPageChoiceState createState() => MainPageChoiceState();
+}
+
+class MainPageChoiceState extends State<MainPageChoiceApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
