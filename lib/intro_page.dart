2c4145a4e3a859ba2706c003fc02418c6042adb0
osiris <osiris@31337.local>
2021-12-09 14:12:10 +0900

    added intro img files

--- lib/intro_page.dart
+++ lib/intro_page.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 
-class IntroPage extends StatefulWidget {
+class IntroPageApp extends StatefulWidget {
+  @override
+  IntroPageState createState() => IntroPageState();
+}
+
+class IntroPageState extends State<IntroPageApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
